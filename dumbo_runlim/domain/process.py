cfac801b5cbdd1701d3eaac8d3e3043b31d9a9c9
Mario Alviano <mario.alviano@gmail.com>
2023-12-23 18:21:13 +0100

    cpu

--- dumbo_runlim/domain/process.py
+++ dumbo_runlim/domain/process.py
@@ -1,6 +1,35 @@
 import dataclasses
+import os
+
+import psutil
+from dumbo_utils import primitives
+from dumbo_utils.validation import validate
 
 
-@dataclasses.dataclass
+@dataclasses.dataclass(frozen=True)
+class CPUAffinity:
+    value: tuple[int, ...] = dataclasses.field(default_factory=lambda: CPUAffinity.cpus())
+
+    def __post_init__(self):
+        cpus = self.cpus()
+        for value in self.value:
+            validate("cpu", value, is_in=cpus, help_msg=f"CPU {value} is invalid")
+
+    @staticmethod
+    def of(*args: int) -> "CPUAffinity":
+        return CPUAffinity(args)
+
+    @staticmethod
+    def cpus() -> tuple[int, ...]:
+        return tuple(psutil.Process(os.getpid()).cpu_affinity())
+
+
+@primitives.bounded_integer(min_value=-20, max_value=20)
+class CPUNice:
+    pass
+
+
+@dataclasses.dataclass(frozen=True)
 class Process:
-    ...
+    cpu_affinity: CPUAffinity = dataclasses.field(default=CPUAffinity())
+    cpu_nice: CPUNice = dataclasses.field(default=CPUNice(20))
---
+++ tests/domain/test_process.py
@@ -0,0 +1,13 @@
+import pytest
+from dumbo_utils.validation import ValidationError
+
+from dumbo_runlim.domain.process import CPUAffinity
+
+
+def test_cpu_affinity_default_to_all():
+    assert CPUAffinity().value == CPUAffinity.cpus()
+
+
+def test_cpu_affinity_must_be_cpu():
+    with pytest.raises(ValidationError):
+        CPUAffinity.of(-1)
