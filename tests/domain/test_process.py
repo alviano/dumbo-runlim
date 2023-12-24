8c729fc2a63a8c8e4fa66125dcebcb0b2d95f4e5
Mario Alviano <mario.alviano@gmail.com>
2023-12-24 10:55:00 +0100

    test resources

--- tests/domain/test_process.py
+++ tests/domain/test_process.py
@@ -1,14 +1,7 @@
-import pytest
-from dumbo_utils.validation import ValidationError
-
-from dumbo_runlim.domain.process import CPUAffinity, Process
+from dumbo_runlim.domain.process import Process
 from dumbo_runlim.domain.resources import Limit
 
 
-def test_cpu_affinity_default_to_all():
-    assert CPUAffinity().value == CPUAffinity.cpus()
-
-
-def test_cpu_affinity_must_be_cpu():
-    with pytest.raises(ValidationError):
-        CPUAffinity.of(-1)
+# def test_process():
+#     Process("sleep 10", report_frequency=1, limit=Limit(real=3)).run()
+#     assert False
