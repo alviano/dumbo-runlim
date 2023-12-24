6bbb858fb1d7db14ef47d2382109faaa462d2334
Mario Alviano <mario.alviano@gmail.com>
2023-12-24 09:07:57 +0100

    progressive samples add callbacks shared lock

--- dumbo_runlim/domain/process.py
+++ dumbo_runlim/domain/process.py
@@ -4,6 +4,7 @@ import subprocess
 import sys
 import threading
 import time
+from typing import Callable
 
 import psutil
 from dumbo_utils.validation import validate
@@ -32,29 +33,50 @@ class CPUAffinity:
 @dataclasses.dataclass(frozen=True)
 class Process:
     command: str
+    tag: str = dataclasses.field(default='')
     cpu_affinity: CPUAffinity = dataclasses.field(default=CPUAffinity())
     cpu_nice: int = dataclasses.field(default=20)
     report_frequency: int = dataclasses.field(default=10)
     limit: Limit = dataclasses.field(default=Limit())
+    stream_callback: Callable[[str, str, bool, UsageSummary], bool] = dataclasses.field(
+        default_factory=lambda: Process._default_stream_callback)
+    report_callback: Callable[[str, UsageSummary], bool] = dataclasses.field(
+        default_factory=lambda: Process._default_report_callback)
+
+    __lock = threading.Lock()
+
+    @staticmethod
+    def _default_stream_callback(tag: str, line: str, is_err: bool, usage_summary: UsageSummary) -> bool:
+        print(f"{tag}[{'e' if is_err else 'o'}{usage_summary.real:12.3f}]", line if line[-1] != '\n' else line[:-1],
+              file=sys.stderr)
+        return False
+
+    @staticmethod
+    def _default_report_callback(tag: str, usage_summary: UsageSummary) -> bool:
+        print(
+            f"[{tag}] "
+            f"time: {usage_summary.real:10.1f} {usage_summary.time:10.1f}; "
+            f"memory: {usage_summary.max_memory:10.0f} {usage_summary.memory:10.0f} {usage_summary.swap:10.0f}"
+        )
+        return False
 
     def __post_init__(self):
         validate("cpu_nice", self.cpu_nice, min_value=-20, max_value=20)
         validate("report_frequency", self.report_frequency, min_value=1, max_value=3600)
 
     def run(self):
-        start_time = time.time()
-        usage_summary = UsageSummary()
-        lock = threading.Lock()
+        usage_key = object()
+        usage_summary = UsageSummary(usage_key)
 
-        def reader(stream, prefix):
+        def reader(stream, *, is_err):
             while True:
                 line = stream.readline().decode()
                 if not line:
                     break
-                real = time.time() - start_time
-                lock.acquire()
-                print(f"[{prefix}{real:12.3f}]", line if line[-1] != '\n' else line[:-1], file=sys.stderr)
-                lock.release()
+                self.__lock.acquire()
+                if self.stream_callback(self.tag, line, is_err, usage_summary):
+                    usage_summary.terminate("interrupted by stream callback", key=usage_key)
+                self.__lock.release()
 
         def waiter():
             waiter.result = process.wait()
@@ -64,24 +86,24 @@ class Process:
                 subprocesses = process.children(recursive=True)
             except psutil.NoSuchProcess:
                 subprocesses = []
-            subprocesses = [p for p in subprocesses if p.cmdline != process.cmdline]
+            subprocesses.append(process)
 
-            for p in subprocesses:
+            for proc in subprocesses:
                 try:
-                    p.terminate()
+                    proc.terminate()
                 except psutil.NoSuchProcess:
                     pass
 
-            gone, alive = psutil.wait_procs(subprocesses, timeout=10)
-            for p in alive:
+            gone, alive = psutil.wait_procs(subprocesses, timeout=1)
+            for proc in alive:
                 try:
-                    p.kill()
+                    proc.kill()
                 except psutil.NoSuchProcess:
                     pass
 
         cmd = self.command.replace('"', '\\"')
         process = psutil.Popen(
-            ["bash", "-c", f"PYTHONHASHSEED=0 trap '' SIGINT SIGTERM; ({cmd})"],
+            ["bash", "-c", f"PYTHONHASHSEED=0 {cmd}"],
             stdout=subprocess.PIPE,
             stderr=subprocess.PIPE
         )
@@ -89,23 +111,28 @@ class Process:
         process.nice(self.cpu_nice)
 
         def sample():
-            if usage_summary.update(process, self.report_frequency):
-                print(
-                    f"time: {usage_summary.real:10.1f} {usage_summary.time:10.1f}; "
-                    f"memory: {usage_summary.max_memory:10.0f} {usage_summary.memory:10.0f} {usage_summary.swap:10.0f}"
-                )
+            if usage_summary.update(process, self.report_frequency, self.limit, key=usage_key):
+                self.__lock.acquire()
+                if self.report_callback(self.tag, usage_summary):
+                    usage_summary.terminate("interrupted by report callback", key=usage_key)
+                self.__lock.release()
 
-        out_reader = threading.Thread(target=lambda: reader(process.stdout, "o"))
-        err_reader = threading.Thread(target=lambda: reader(process.stderr, "e"))
+        out_reader = threading.Thread(target=lambda: reader(process.stdout, is_err=False))
+        err_reader = threading.Thread(target=lambda: reader(process.stderr, is_err=True))
         wait = threading.Thread(target=waiter)
         out_reader.start()
         err_reader.start()
         wait.start()
         while wait.is_alive():
-            time.sleep(.1)
-            if usage_summary.exceed_limit(self.limit):
-                kill()
+            time.sleep(
+                0.10 if usage_summary.real < 1 else
+                0.25 if usage_summary.real < 3 else
+                0.50 if usage_summary.real < 10 else
+                1.00
+            )
             sample()
+            if usage_summary.terminated:
+                kill()
         wait.join()
         out_reader.join()
         err_reader.join()
--- dumbo_runlim/domain/resources.py
+++ dumbo_runlim/domain/resources.py
@@ -55,11 +55,13 @@ class Usage:
 @typeguard.typechecked
 @dataclasses.dataclass()
 class UsageSummary:
+    __key: object = dataclasses.field()
     __start: float = dataclasses.field(default=time.time(), init=False)
     __real: float = dataclasses.field(default=0, init=False)
     __max_memory: float = dataclasses.field(default=0, init=False)
     __processes: dict[int, Usage] = dataclasses.field(default_factory=lambda: defaultdict(Usage), init=False)
     __last_report: float = dataclasses.field(default=0, init=False)
+    __status: str = dataclasses.field(default="running", init=False)
 
     @property
     def real(self) -> float:
@@ -81,13 +83,29 @@ class UsageSummary:
     def swap(self) -> float:
         return sum(process.swap for process in self.__processes.values())
 
-    def exceed_limit(self, limit: Limit) -> bool:
-        return (self.real > limit.realtime or
-                self.time > limit.time or
-                self.max_memory > limit.memory or
-                self.swap > limit.swap)
+    @property
+    def status(self) -> str:
+        return self.__status
 
-    def update(self, process: psutil.Process, report_frequency: int) -> bool:
+    @property
+    def terminated(self) -> bool:
+        return self.__status != "running"
+
+    def __exceed_limit(self, limit: Limit) -> bool:
+        if self.real > limit.realtime:
+            self.terminate("out of time (real)", key=self.__key)
+        elif self.time > limit.time:
+            self.terminate("out of time", key=self.__key)
+        elif self.max_memory > limit.memory:
+            self.terminate("out of memory", key=self.__key)
+        elif self.swap > limit.swap:
+            self.terminate("out of memory (swap)", key=self.__key)
+        else:
+            return False
+        return True
+
+    def update(self, process: psutil.Process, report_frequency: int, limit: Limit, *, key: object) -> bool:
+        validate("key", key, equals=self.__key)
         all_processes = [process]
         try:
             all_processes.extend(process.children(recursive=True))
@@ -103,7 +121,13 @@ class UsageSummary:
         self.__real = time.time() - self.__start
         self.__max_memory = max(self.__max_memory, self.memory)
 
-        if self.real > self.__last_report + report_frequency:
+        if self.__exceed_limit(limit) or self.real > self.__last_report + report_frequency:
             self.__last_report = self.real
             return True
         return False
+
+    def terminate(self, status: str, *, key: object) -> None:
+        validate("key", key, equals=self.__key)
+        validate("status", self.__status, equals="running")
+        validate("status", status != self.__status, equals=True)
+        self.__status = status
